718d3b4c4e4307bba2da2e0e02dc828d3d20e4a3
MarcoGotti <mrc.gotti@gmail.com>
2024-06-06 21:47:55 +0000

    create model Project -mcrsR, update ProjectController, set routes:resource

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+//use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
