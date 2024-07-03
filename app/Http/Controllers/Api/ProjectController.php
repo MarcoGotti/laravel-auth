534766f9ff9f3f1365c22eccc9ac390cd470daf5
MarcoGotti <mrc.gotti@gmail.com>
2024-07-03 11:41:05 +0000

    set api routes index and show, create api/projectController and transfer methods index and show

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::with(['type', 'technologies'])->orderByDesc('id')->paginate(10);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show($id)
+    {
+
+        $project = Project::with(['type', 'technologies'])->where('id', $id)->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'result' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'result' => '404 Not found'
+            ]);
+        }
+    }
+}
